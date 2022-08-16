89ccbde84c124dd9741b042d7d15bc46e081fdaf
ali norouzi <a.norouzimanesh@gmail.com>
2022-08-16 17:19:09 +0430

    Updated src/create.py. Added 2 new classes

---
+++ src/create.py
@@ -0,0 +1,31 @@
+'''
+    This file will handle all orders for creating a project
+'''
+
+'''
+    Creator: Ali Norouzi
+    Last modified: Tue 5:05 PM, August 16, 2022
+    changes: ?
+'''
+
+import json
+
+# import my .py module
+import common
+
+
+class CreateFiles:
+    '''this class will create all files for a new project'''
+    pass
+
+
+class CreateProject:
+    '''this class will create a new project(directory)'''
+
+    # get the name of the new project
+    def get_name(self, name):
+        '''get the name of a new project'''
+        
+        
+    # check if the name doesn't exist (handle existance)
+    # create settings.json, wrong.json, words.json
